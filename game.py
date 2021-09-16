a33a6dfc5749c927747612c8c65e84e53d94305b
rachelmk96 <89559270+rachelmk96@users.noreply.github.com>
2021-09-16 12:08:11 -0400

    new changes

---
+++ game.py
@@ -0,0 +1,6 @@
+#starting prompt
+print("Rock, Paper, Scissors, Shoot!")
+#user input
+user_choice = input("Choose 'rock' or 'paper' or 'scissors': ")
+#computer choice at random
+options = ["rock", "paper", "scissors"]
---
+++ mygame.py
@@ -0,0 +1,10 @@
+#game start prompt
+print("-------------------")
+print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
+print("-------------------")
+user_choice = input("Please choose either 'rock', 'paper', or 'scissors': ")
+
+#user input
+
+print("You chose: ")
+print(user_choice)
