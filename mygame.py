c7f2b4fef902e6d010849f246cc2f7bdae1d4301
rachelmk96 <89559270+rachelmk96@users.noreply.github.com>
2021-09-21 11:18:00 -0400

    Update mygame.py

--- mygame.py
+++ mygame.py
@@ -1,18 +1,24 @@
 #game start prompt
 print("-------------------\n Welcome 'Player One' to my Rock-Paper-Scissors game...")
 print("-------------------")
-user_choice = input("Please choose either 'rock', 'paper', or 'scissors': ")
 
 #user input
+user_choice = input("Please choose either 'rock', 'paper', or 'scissors': ")
 
-print("You chose: ")
-print(user_choice)
+#invalid user input prompt
+if user_choice != "rock" or "paper" or "scissors": #?????????
+    print("Invalid input friend! Use lower caps. Exiting game...")
+    exit()
 
-#comupter computing
+#comupter computing random output
 import random
 options = ["rock", "paper", "scissors"]
 
 computer_choice = random.choice(options)
+
+#user output
+print("You chose: ")
+print(user_choice)
 
 #computer output
 print("Computer chose: ")
