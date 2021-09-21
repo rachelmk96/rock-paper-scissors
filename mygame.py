44d1566dc1aec8da0525c9a4a6abcf6982d102f5
rachelmk96 <89559270+rachelmk96@users.noreply.github.com>
2021-09-21 12:11:16 -0400

    update mygame.py

--- mygame.py
+++ mygame.py
@@ -5,16 +5,17 @@ print("-------------------")
 #user input
 user_choice = input("Please choose either 'rock', 'paper', or 'scissors': ")
 
-#invalid user input prompt
-if user_choice != "rock" or "paper" or "scissors": #?????????
-    print("Invalid input friend! Use lower caps. Exiting game...")
-    exit()
-
 #comupter computing random output
 import random
 options = ["rock", "paper", "scissors"]
 
 computer_choice = random.choice(options)
+
+#invalid user input prompt
+# User option validation adapted shared by Gianna Valencia from Slack
+if user_choice not in options:
+    print("Not a valid option friend. Please use lower case. Exiting...")
+    exit()
 
 #user output
 print("You chose: ")
@@ -23,3 +24,11 @@ print(user_choice)
 #computer output
 print("Computer chose: ")
 print(computer_choice)
+
+#determining winner
+if user_choice == computer_choice:
+  print("It's a tie!")
+#debugging
+breakpoint()
+
+print("Thanks for playing friend. Please play again!")
